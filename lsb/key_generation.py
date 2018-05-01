452a946a294d610d387fde0ceffbdbc033a6a8b9
forscienceandpie <38381721+forscienceandpie@users.noreply.github.com>
2018-05-01 11:24:40 +0300

    Add files via upload

---
+++ lsb/key_generation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3 
+
+import sys
+import os
+
+def main():
+    try:
+        file_ = open(sys.argv[1], "r")
+    except FileNotFoundError:
+        print("\_0_0_/")
+        exit(2)
+    dump = len(file_.read())
+    print(dump)
+    file_.close()
+
+if len(sys.argv) != 2:
+    print("\_0_0_/")
+    exit(1)
+
+if __name__ == "__main__":
+    main()
